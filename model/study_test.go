c315eb6e93a6c625011ccb01724004297006aaf6
Ben Selby <ben.selby@prolific.co>
2023-05-21 09:30:19 +0100

    More tests for the study model.

---
+++ model/study_test.go
@@ -0,0 +1,75 @@
+package model_test
+
+import (
+	"testing"
+
+	"github.com/prolific-oss/prolificli/model"
+)
+
+func TestFilterValueReturnsName(t *testing.T) {
+	name := "Patterns of migratory birds"
+	study := model.Study{
+		ID:   "study-id",
+		Name: name,
+	}
+
+	if study.FilterValue() != name {
+		t.Fatalf("expected filter value to be %s, got %s", name, study.FilterValue())
+	}
+}
+
+func TestTitleIsTheStudyName(t *testing.T) {
+	name := "Patterns of migratory birds"
+	study := model.Study{
+		ID:   "study-id",
+		Name: name,
+	}
+
+	if study.Title() != name {
+		t.Fatalf("expected filter value to be %s, got %s", name, study.Title())
+	}
+}
+
+func TestDescriptionReturnsADescriptiveString(t *testing.T) {
+	study := model.Study{
+		ID:                   "study-id",
+		Name:                 "Patterns of migratory birds",
+		Status:               model.StatusActive,
+		StudyType:            "single",
+		TotalAvailablePlaces: 515,
+		Desc:                 "This study is going to be longitudinal study around bird migration",
+	}
+
+	expected := "active - single - 515 places available - This study is going to be longitudinal study around bird migration"
+	if study.Description() != expected {
+		t.Fatalf("expected filter value to be %s, got %s", expected, study.Description())
+	}
+}
+
+func TestGetCurrencyCodeCanFigureOutWhichCurrencyToUse(t *testing.T) {
+	tt := []struct {
+		Name                    string
+		PresentmentCurrencyCode string
+		CurrencyCode            string
+		Expected                string
+	}{
+		{"Using presentment", "USD", "GBP", "USD"},
+		{"Using currency", "", "EUR", "EUR"},
+		{"Default", "", "", "GBP"},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.Name, func(t *testing.T) {
+			study := model.Study{
+				PresentmentCurrencyCode: tc.PresentmentCurrencyCode,
+				CurrencyCode:            tc.CurrencyCode,
+			}
+
+			actual := study.GetCurrencyCode()
+
+			if actual != tc.Expected {
+				t.Fatalf("expected %v, got %v", tc.Expected, actual)
+			}
+		})
+	}
+}
