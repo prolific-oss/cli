2f7b93491e08218a1e0434bd9c360d4d43f69e7b
Ben Selby <ben.selby@prolific.co>
2023-05-21 09:19:12 +0100

    Addition of more tests for the hooks command package.

--- cmd/hook/list_test.go
+++ cmd/hook/list_test.go
@@ -1,14 +1,18 @@
 package hook_test
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
 	"os"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/prolific-oss/prolificli/client"
 	"github.com/prolific-oss/prolificli/cmd/hook"
 	"github.com/prolific-oss/prolificli/mock_client"
+	"github.com/prolific-oss/prolificli/model"
 )
 
 func TestNewListCommand(t *testing.T) {
@@ -74,5 +78,47 @@ func TestNewListCommandCanAskForDisabledHooks(t *testing.T) {
 
 	if err.Error() != expected {
 		t.Fatalf("expected\n'%s'\ngot\n'%s'\n", expected, err.Error())
+	}
+}
+
+func TestNewListCommandCallsTheAPI(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	c := mock_client.NewMockAPI(ctrl)
+
+	response := client.ListHooksResponse{
+		Results: []model.Hook{
+			{
+				ID:          "hook-id",
+				EventType:   "wibble",
+				TargetURL:   "https://app.prolific.co",
+				IsEnabled:   true,
+				WorkspaceID: "workspace-id",
+			},
+		},
+	}
+
+	c.
+		EXPECT().
+		GetHooks(gomock.Eq(false)).
+		Return(&response, nil).
+		AnyTimes()
+
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+
+	cmd := hook.NewListCommand("list", c, writer)
+	_ = cmd.Flags().Set("disabled", "true")
+	_ = cmd.RunE(cmd, nil)
+
+	writer.Flush()
+
+	actual := b.String()
+	expected := `ID      Event  Target URL              Enabled Workspace ID
+hook-id wibble https://app.prolific.co true    workspace-id
+`
+
+	if actual != expected {
+		t.Fatalf("expected\n'%s'\ngot\n'%s'\n", expected, actual)
 	}
 }
