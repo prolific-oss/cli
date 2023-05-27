59e17998cfd94d25decc430ea4a76dc160568c0e
Ben Selby <ben.selby@prolific.co>
2023-05-27 21:15:27 +0100

    Provide long/example docs for `workspace` and `project` commands

--- cmd/project/project.go
+++ cmd/project/project.go
@@ -12,6 +12,9 @@ func NewProjectCommand(client client.API, w io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "project",
 		Short: "Manage and view your projects in a workspace",
+		Long: `Manage your projects
+Projects are a way to organise studies in a workspace.
+`,
 	}
 
 	cmd.AddCommand(
