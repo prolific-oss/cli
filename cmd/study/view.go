82a5ebc14790ebb22383c378c6ccc7f9ee918262
Ben Selby <ben.selby@prolific.co>
2023-05-28 18:56:41 +0100

    Provide long/example definitions for the `study view` command

--- cmd/study/view.go
+++ cmd/study/view.go
@@ -15,7 +15,11 @@ func NewViewCommand(client client.API, w io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "view",
 		Short: "Provide details about your study, requires a Study ID",
-		Args:  cobra.MinimumNArgs(1),
+		Long:  `View study details`,
+		Example: `
+To get details about a study
+$ prolific study view 64395e9c2332b8a59a65d51e`,
+		Args: cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			study, err := client.GetStudy(args[0])
 			if err != nil {
