6def58f3f7327b01fc87501b7396d9db80e93849
Ben Selby <ben.selby@prolific.co>
2023-05-28 18:59:46 +0100

    Provide long/example definitions for the `study duplicate` command

--- cmd/study/duplicate.go
+++ cmd/study/duplicate.go
@@ -14,7 +14,15 @@ func NewDuplicateCommand(client client.API, w io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "duplicate",
 		Short: "Duplicate an existing study",
-		Args:  cobra.MinimumNArgs(1),
+		Long: `Duplicate an existing study
+
+This may be useful if you have a templated study in the web application. If you
+are mainly using the CLI, you can define a JSON/YAML file to pass into the "study
+create" command`,
+		Example: `
+To duplicate a study, you need the ID
+$ prolific study duplicate 64395e9c2332b8a59a65d51e`,
+		Args: cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			study, err := client.DuplicateStudy(args[0])
 			if err != nil {
