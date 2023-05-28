2ff09b92064a5a10e5bb202ca9e269c7c824345c
Ben Selby <ben.selby@prolific.co>
2023-05-28 18:28:27 +0100

    Provide long/example definitions for the submission commands

--- cmd/submission/list.go
+++ cmd/submission/list.go
@@ -29,6 +29,39 @@ func NewListCommand(c client.API, w io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "Provide details about your submissions, requires Study ID",
+		Long: `List submissions for a given study
+
+A published study will have submissions taken by the Prolific Participants. This
+commands allows you to list those submissions.`,
+		Example: `
+You can list all the submissions for a given study
+$ prolific submission list -s 63c123af913a974f87e8e7fc
+
+You can use the paging options to limit the submissions returned, for example 5
+$ prolific submission list -s 63c123af913a974f87e8e7fc -l 5
+
+You can also offset the results, for example skipping 5
+$ prolific submission list -s 63c123af913a974f87e8e7fc -l 5 -o 5
+
+You can render the results as a CSV format
+$ prolific submission list -s 63c123af913a974f87e8e7fc -l 5 -c
+
+You can specify the fields you want to render in either the standard or CSV view
+$ prolific submission list -s 63c123af913a974f87e8e7fc -f ID,Status,TimeTaken
+
+The fields you can use are
+- ID
+- ParticipantID
+- StartedAt
+- CompletedAt
+- IsComplete
+- TimeTaken
+- Reward
+- Status
+- StudyCode
+- StarAwarded
+- BonusPayments
+- IP`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			opts.Args = args
 
--- cmd/submission/submission.go
+++ cmd/submission/submission.go
@@ -12,6 +12,11 @@ func NewSubmissionCommand(client client.API, w io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "submission",
 		Short: "Manage and view your study submissions",
+		Long: `Manage study submissions
+
+A published study will have submissions taken by the Prolific Participants. These
+commands allow you to manage those submissions.
+`,
 	}
 
 	cmd.AddCommand(
