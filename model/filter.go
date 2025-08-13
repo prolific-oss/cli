223560a55cb715359956d6dc97198c140d2af4f4
Ben Selby <ben.selby@prolific.com>
2025-08-13 04:45:17 +0100

    Provide more context for the filter view command

--- model/filter.go
+++ model/filter.go
@@ -2,14 +2,18 @@ package model
 
 // Filter holds information about the filter that makes up a filter set
 type Filter struct {
-	ID                string      `json:"id" mapstructure:"id"`
-	FilterID          string      `json:"filter_id" mapstructure:"filter_id"`
-	FilterTitle       string      `json:"title" mapstructure:"title"`
-	FilterDescription string      `json:"description" mapstructure:"description"`
-	Question          string      `json:"question" mapstructure:"question"`
-	Type              string      `json:"type" mapstructure:"type"`
-	SelectedValues    []string    `json:"selected_values,omitempty" mapstructure:"selected_values"`
-	SelectedRange     FilterRange `json:"selected_range,omitempty" mapstructure:"selected_range"`
+	ID                string            `json:"id" mapstructure:"id"`
+	FilterID          string            `json:"filter_id" mapstructure:"filter_id"`
+	FilterTitle       string            `json:"title" mapstructure:"title"`
+	FilterDescription string            `json:"description" mapstructure:"description"`
+	Question          string            `json:"question" mapstructure:"question"`
+	Type              string            `json:"type" mapstructure:"type"`
+	DataType          string            `json:"data_type" mapstructure:"data_type"`
+	Min               interface{}       `json:"min,omitempty" mapstructure:"min"`
+	Max               interface{}       `json:"max,omitempty" mapstructure:"max"`
+	Choices           map[string]string `json:"choices,omitempty" mapstructure:"choices"`
+	SelectedValues    []string          `json:"selected_values,omitempty" mapstructure:"selected_values"`
+	SelectedRange     FilterRange       `json:"selected_range,omitempty" mapstructure:"selected_range"`
 }
 
 // FilterRange holds the lower and upper bounds of a filter
