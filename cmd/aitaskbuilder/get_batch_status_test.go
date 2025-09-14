024b46330885989593746f411b74f0f4596a2f85
SeanAlexanderHarris <sean.alexander.harris.29@googlemail.com>
2025-09-14 22:12:04 +0100

    refactor(#203): :recycle: use model enum on mock response
    fair minor point prompted by copilot for consistency

--- cmd/aitaskbuilder/get_batch_status_test.go
+++ cmd/aitaskbuilder/get_batch_status_test.go
@@ -43,7 +43,7 @@ func TestNewGetBatchStatusCommandCallsAPI(t *testing.T) {
 
 	response := client.GetAITaskBuilderBatchStatusResponse{
 		AITaskBuilderBatchStatus: model.AITaskBuilderBatchStatus{
-			Status: "UNINITIALISED",
+			Status: model.AITaskBuilderBatchStatusUninitialised,
 		},
 	}
 
