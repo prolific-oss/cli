d64995227ca83375d5ebf0566d2fecb746802a2c
SeanAlexanderHarris <sean.alexander.harris.29@googlemail.com>
2025-11-01 20:52:17 +0000

    #226 feature: :fire: add create dataset command to aitaskbuilder

--- cmd/aitaskbuilder/constants.go
+++ cmd/aitaskbuilder/constants.go
@@ -2,7 +2,10 @@ package aitaskbuilder
 
 const (
 	// Error messages
-	ErrBatchIDRequired   = "batch ID is required"
-	ErrBatchNotFound     = "batch not found"
-	ErrDatasetIDRequired = "dataset ID is required"
+	ErrBatchIDRequired     = "batch ID is required"
+	ErrBatchNotFound       = "batch not found"
+	ErrDatasetIDRequired   = "dataset ID is required"
+	ErrDatasetNotFound     = "dataset not found"
+	ErrNameRequired        = "name is required"
+	ErrWorkspaceIDRequired = "workspace ID is required"
 )
