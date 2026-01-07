fec179f7acc97cb99452ec6aa66ac3a9b88f3316
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2026-01-07 12:33:41 +0000

    chore(DCP-2157): add format command and format

--- cmd/collection/constants.go
+++ cmd/collection/constants.go
@@ -2,8 +2,8 @@ package collection
 
 const (
 	// Error messages
-	ErrCollectionItemsRequired       = "at least one item must be provided"
-	ErrWorkspaceIDRequired           = "workspace ID is required"
-	ErrNameRequired                  = "name is required"
-	ErrWorkspaceNotFound             = "workspace not found"
+	ErrCollectionItemsRequired = "at least one item must be provided"
+	ErrWorkspaceIDRequired     = "workspace ID is required"
+	ErrNameRequired            = "name is required"
+	ErrWorkspaceNotFound       = "workspace not found"
 )
